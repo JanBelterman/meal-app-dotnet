4f430a312ac583e17d7f94a08f111b87be7f6667
Jan Belterman <jjabelte@avans.nl>
2018-09-11 23:00:54 +0200

    Some configuration

--- MaaltijdApplicatie/Startup.cs
+++ MaaltijdApplicatie/Startup.cs
@@ -1,28 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MaaltijdApplicatie {
+
     public class Startup {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        // Adding services to the container
         public void ConfigureServices(IServiceCollection services) {
+
+            // Add MVC
+            services.AddMvc();
+
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        // Configure http request pipeline
         public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+
+            // Use developer exception pages if the application is in development mode
             if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) => {
-                await context.Response.WriteAsync("Hello World!");
+            app.UseStatusCodePages();
+            app.UseStaticFiles();
+
+            // Configure http request routes
+            app.UseMvc(routes => {
+
+
+
             });
+
         }
+
     }
+
 }
