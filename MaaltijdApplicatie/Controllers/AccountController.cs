51644f6b6f7d9fc595640feca6433cf8832a3a4a
Jan Belterman <jjabelte@avans.nl>
2018-09-12 17:42:34 +0200

    Added account logic to login and logout

--- MaaltijdApplicatie/Controllers/AccountController.cs
+++ MaaltijdApplicatie/Controllers/AccountController.cs
@@ -1,17 +1,21 @@
 ﻿using MaaltijdApplicatie.Models.Domain;
 using MaaltijdApplicatie.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace MaaltijdApplicatie.Controllers {
 
+    [Authorize]
     public class AccountController : Controller {
 
         private UserManager<AppUser> userManager;
+        private SignInManager<AppUser> signInManager;
 
-        public AccountController(UserManager<AppUser> userManager) {
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager) {
             this.userManager = userManager;
+            this.signInManager = signInManager;
         }
 
         public ViewResult Create() {
@@ -44,6 +48,49 @@ namespace MaaltijdApplicatie.Controllers {
 
             return View(register);
 
+        }
+        
+        [AllowAnonymous]
+        public ViewResult Login() {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(AppUserViewModel loginModel) {
+
+            // If login form is valid
+            if (ModelState.IsValid) {
+
+                // Get user
+                AppUser user = await userManager.FindByNameAsync(loginModel.Username);
+
+                // If user is found:
+                if (user != null) {
+
+                    // Sign user out from different sessions
+                    await signInManager.SignOutAsync();
+                    // Attempt to sign user in & when succeeded: redirect user to /Account/Index
+                    if ((await signInManager.PasswordSignInAsync(user,
+                        loginModel.Password, false, false)).Succeeded) {
+                        return Redirect("/Admin/Index");
+                    }
+
+                }
+
+            }
+
+            // Pass error message to view
+            ModelState.AddModelError("", "Ongeldige gebruikersnaam of wachtwoord");
+            return View(loginModel);
+
+        }
+
+        // Logs user out
+        public async Task<RedirectResult> Logout(string returnUrl = "/") {
+            await signInManager.SignOutAsync();
+            return Redirect(returnUrl);
         }
 
     }
